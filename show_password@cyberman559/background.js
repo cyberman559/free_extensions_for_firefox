134db3f023518acaa1300cf5b886e785520c8f33
cyberman559 <cyberman559@gmail.com>
2025-10-29 14:51:23 +0300

    показать/скрыть

--- show_password@cyberman559/background.js
+++ show_password@cyberman559/background.js
@@ -1,19 +1,55 @@
+// Создаём оба пункта один раз
 browser.contextMenus.create({
-    id: "lccbm559-9b4a-4d6f-bb9b-6f9c9c0a9b9c",
+    id: "cbm559-show-password",
     title: "Показать пароль",
-    contexts: ["editable"]
+    contexts: ["editable"],
+    visible: false
 });
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "lccbm559-9b4a-4d6f-bb9b-6f9c9c0a9b9c") {
-        const code = `
-            if (document.activeElement && document.activeElement.tagName === 'INPUT' && document.activeElement.type === 'password') {
-                document.activeElement.type = 'text';
-            }
-        `;
+browser.contextMenus.create({
+    id: "cbm559-hide-password",
+    title: "Скрыть как пароль",
+    contexts: ["editable"],
+    visible: false
+});
 
+// Срабатывает при открытии контекстного меню
+browser.contextMenus.onShown.addListener(async (info, tab) => {
+    try {
+        const [type] = await browser.tabs.executeScript(tab.id, {
+            code: `
+                (function() {
+                    var el = document.activeElement;
+                    return el && el.tagName === 'INPUT' ? el.type : null;
+                })();
+            `
+        });
+
+        await browser.contextMenus.update("cbm559-show-password", { visible: type === "password" });
+        await browser.contextMenus.update("cbm559-hide-password", { visible: type === "text" });
+        browser.contextMenus.refresh();
+    } catch (e) {
+        console.warn("Нет разрешения на доступ к вкладке:", e);
+    }
+});
+
+// Обработка кликов по пунктам меню
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "cbm559-show-password") {
         browser.tabs.executeScript(tab.id, {
-            code: code
+            code: `
+                var el = document.activeElement;
+                if (el && el.tagName === 'INPUT' && el.type === 'password') el.type = 'text';
+            `
+        });
+    }
+
+    if (info.menuItemId === "cbm559-hide-password") {
+        browser.tabs.executeScript(tab.id, {
+            code: `
+                var el = document.activeElement;
+                if (el && el.tagName === 'INPUT' && el.type === 'text') el.type = 'password';
+            `
         });
     }
 });
